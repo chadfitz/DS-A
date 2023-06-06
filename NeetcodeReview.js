2215868b298a8f4201b381d446eda55030b3c962
chad fitzgerald <fitzchad@gmail.com>
2023-06-06 12:02:44 -0700

    Finish problem

--- NeetcodeReview.js
+++ NeetcodeReview.js
@@ -11,3 +11,23 @@ var containsDuplicate = function(nums) {
 
   return false;
 };
+
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+var isAnagram = function(str1, str2) {
+  let counts = {};
+
+  for (let i = 0; i < str1.length; i++) {
+      if (counts[str1[i]] === undefined) counts[str1[i]] = 1
+      else counts[str1[i]] += 1;
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+      if (counts[str2[i]] === undefined) return false
+      else counts[str2[i]] -= 1;
+  }
+
+  return Object.values(counts).every( num => num === 0)
+};
