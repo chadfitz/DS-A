5f72ccf18d3005066b620b78e213a974e8298a5f
chad fitzgerald <fitzchad@gmail.com>
2023-06-07 13:01:01 -0700

    Finish problem

--- NeetcodeReview.js
+++ NeetcodeReview.js
@@ -31,3 +31,18 @@ var isAnagram = function(str1, str2) {
 
   return Object.values(counts).every( num => num === 0)
 };
+
+// 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+var twoSum = function(nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+      if (hash[nums[i]] === undefined) hash[target - nums[i]] = i;
+      else return [hash[nums[i]], i]
+  }
+}
