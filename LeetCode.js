b24dc078d04f45696343a3d6b3bd93136ae3d419
chad fitzgerald <fitzchad@gmail.com>
2023-06-22 14:31:35 -0700

    Solve Problem

--- LeetCode.js
+++ LeetCode.js
@@ -571,3 +571,18 @@ var findAnagrams = function(s, p) {
 
   return res;
 };
+
+// 1047. Remove All Adjacent Duplicates In String
+var removeDuplicates = function(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === stack[stack.length - 1]) {
+          stack.pop()
+      } else {
+          stack.push(s[i])
+      }
+  }
+
+  return stack.join("");
+};
