7a56faf12f2710ed0b3ce05c6a53d7379533b192
chad fitzgerald <fitzchad@gmail.com>
2023-11-17 14:04:11 -0800

    Finish problem

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -96,3 +96,18 @@ class Solution {
         return stack.size() == 0;
     }
 }
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+}
