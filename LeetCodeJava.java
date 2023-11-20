009aa5553fb0652f6a990ce46a1bc413d5c14b65
chad fitzgerald <fitzchad@gmail.com>
2023-11-20 13:06:29 -0800

    Solve problem

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -111,3 +111,15 @@ class Solution {
         return j;
     }
 }
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+
+        return false;
+    }
+}
