c24414a9fcb23eb69dde5a88eb043d7cacfee039
chad fitzgerald <fitzchad@gmail.com>
2023-11-30 15:27:04 -0800

    Solve problem

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -139,6 +139,22 @@ class Solution {
     }
 }
 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
+
 // 217. Contains Duplicate
 class Solution {
     public boolean containsDuplicate(int[] nums) {
