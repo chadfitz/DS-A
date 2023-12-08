33a025fac3abe0bcb661be68718e17420f694937
chad fitzgerald <fitzchad@gmail.com>
2023-12-08 09:43:47 -0800

    Remove duplicate

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -139,18 +139,6 @@ class Solution {
     }
 }
 
-// // 125. Valid Palindrome
-class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != s.charAt(s.length()-1-i)) return false;
-        }
-        return true;
-    }
-}
-
 // 206. Reverse Linked List
 class Solution {
     public ListNode reverseList(ListNode head) {
