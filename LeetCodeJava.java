5114fcc3823302e9c060f85e7ef0e8572e5b9431
chad fitzgerald <fitzchad@gmail.com>
2023-12-13 15:21:30 -0800

    Finish problem

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -198,4 +198,29 @@ class Solution {
     }
 }
 
-// add notes
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> myHashMap = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (myHashMap.get(s.charAt(i)) == null) {
+                myHashMap.put(s.charAt(i), 1);
+            } else {
+                myHashMap.put(s.charAt(i), myHashMap.get(s.charAt(i)) + 1);
+            }
+
+            if (myHashMap.get(t.charAt(i)) == null) {
+                myHashMap.put(t.charAt(i), -1);
+            } else {
+                myHashMap.put(t.charAt(i), myHashMap.get(t.charAt(i)) - 1);
+            }
+        }
+
+        for (int value : myHashMap.values()) {
+            if (value != 0) return false;
+        }
+
+        return true;
+    }
+}
