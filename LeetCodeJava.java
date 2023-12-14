ea2fee8177a27a788c66116acaa349fb769e0b65
chad fitzgerald <fitzchad@gmail.com>
2023-12-14 14:08:18 -0800

    Organize

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -140,18 +140,6 @@ class Solution {
     }
 }
 
-// 217. Contains Duplicate
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) return true;
-        }
-
-        return false;
-    }
-}
 // 206. Reverse Linked List
 class Solution {
     public ListNode reverseList(ListNode head) {
@@ -169,6 +157,18 @@ class Solution {
     }
 }
 
+// 217. Contains Duplicate
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+
+        return false;
+    }
+}
 
 // 242. Valid Anagram
 class Solution {
