8a4e1940e204239d27a9c118383444686c983e1b
chad fitzgerald <fitzchad@gmail.com>
2023-12-20 08:39:52 -0800

    Solve problem

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -197,3 +197,25 @@ class Solution {
         return true;
     }
 }
+
+// 704. Binary Search
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int midpoint = left + (right - left)/2;
+
+            if (target == nums[midpoint]) {
+                return midpoint;
+            } else if (target < nums[midpoint]) {
+                right = midpoint - 1;
+            } else {
+                left = midpoint + 1;
+            }
+        }
+
+        return -1;
+    }
+}
