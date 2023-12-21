c6c837fc555b16904e8cc9ce50e51fdb708e1fa6
chad fitzgerald <fitzchad@gmail.com>
2023-12-21 13:21:05 -0800

    Finish problem

--- LeetCodeJava.java
+++ LeetCodeJava.java
@@ -98,6 +98,33 @@ class Solution {
     }
 }
 
+// 21. Merge Two Sorted Lists
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode preHead = new ListNode();
+        ListNode current = preHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 == null) {
+            current.next = list2;
+        } else {
+            current.next = list1;
+        }
+
+        return preHead.next;
+    }
+}
+
 // 26. Remove Duplicates from Sorted Array
 class Solution {
     public int removeDuplicates(int[] nums) {
