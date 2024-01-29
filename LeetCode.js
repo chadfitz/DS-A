bc0ee4d1ef0f3a20b48163e95ac2d60eb17c2564
chad fitzgerald <fitzchad@gmail.com>
2024-01-29 12:55:17 -0800

    Finish problem

--- LeetCode.js
+++ LeetCode.js
@@ -456,6 +456,27 @@ var isSubsequence = function(s, t) {
   return true;
 };
 
+// 209. Minimum Size Subarray Sum
+var minSubArrayLen = function(target, nums) {
+  let subArr = Infinity;
+  let left = 0;
+  let sum = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+      if (nums[right] >= target) return 1;
+
+      sum += nums[right];
+      console.log("sum: ", sum);
+      while (sum >= target) {
+          subArr = Math.min(subArr, right - left + 1);
+          sum -= nums[left];
+          left++
+      }
+  }
+
+  return subArr === Infinity ? 0 : subArr;
+};
+
 // 217. Contains Duplicate
 var containsDuplicate = function(nums) {
   let count = {}
