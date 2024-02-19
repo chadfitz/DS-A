a9e87477b7a686a2d1fd811485913003a65065cb
chad fitzgerald <fitzchad@gmail.com>
2024-02-19 13:51:07 -0800

    Solve Problem

--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -18,3 +18,24 @@ var minSubArrayLen = function(target, nums) {
 
   return subArr === Infinity ? 0 : subArr;
 };
+
+// 73. Set Matrix Zeroes
+var setZeroes = function(matrix) {
+  let rows = new Set();
+  let columns = new Set();
+
+  for (let r = 0; r < matrix.length; r++) {
+      for (let c = 0; c < matrix[0].length; c++) {
+          if (matrix[r][c] === 0) {
+              rows.add(r);
+              columns.add(c);
+          }
+      }
+  }
+
+  for (let r = 0; r < matrix.length; r++) {
+      for (let c = 0; c < matrix[0].length; c++) {
+          if (rows.has(r) || columns.has(c)) matrix[r][c] = 0;
+      }
+  }
+};
