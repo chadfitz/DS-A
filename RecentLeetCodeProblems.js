84fdb58875ef1910fc0142e94ce8bb55b591c0ea
chad fitzgerald <fitzchad@gmail.com>
2024-02-23 23:18:19 -0800

    Finish problem

--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -106,3 +106,24 @@ var containsNearbyDuplicate = function(nums, k) {
 
     return false;
 };
+
+// 209. Minimum Size Subarray Sum
+var minSubArrayLen = function(target, nums) {
+    let subArr = Infinity;
+    let left = 0;
+    let sum = 0;
+  
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] >= target) return 1;
+  
+        sum += nums[right];
+        console.log("sum: ", sum);
+        while (sum >= target) {
+            subArr = Math.min(subArr, right - left + 1);
+            sum -= nums[left];
+            left++
+        }
+    }
+  
+    return subArr === Infinity ? 0 : subArr;
+  };
