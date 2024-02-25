55044c4010e22f527d68a0ae3dc384098efaf438
chad fitzgerald <fitzchad@gmail.com>
2024-02-25 13:03:42 -0800

    Solve problem

--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -149,3 +149,18 @@ var topKFrequent = function(nums, k) {
 
     return res;
 };
+
+// 206. Reverse Linked List
+var reverseList = function(head) {
+    let current = head;
+    let prev = null;
+
+    while (current) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+};
