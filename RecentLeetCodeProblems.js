6cfe8e8a3bc71d6b56b28548e20658195a8e093f
chad fitzgerald <fitzchad@gmail.com>
2024-02-28 12:43:25 -0800

    Solve problem

--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -215,3 +215,25 @@ var lengthOfLongestSubstring = function(s) {
 
     return longest;
 }
+
+// 347. Top K Frequent Elements
+var topKFrequent = function(nums, k) {
+    const counts = new Map();
+ 
+    for (let num of nums) {
+        counts.set(num, (counts.get(num) || 0) + 1);
+    }
+ 
+    const sorted = [...counts].sort((a, b) => b[1] - a[1]);
+    let result = [];
+    for (let pair of sorted) {
+        if (k > 0) {
+            result.push(pair[0]);
+            k--;
+        } else {
+            break;
+        }
+    }
+ 
+    return result;
+ };
