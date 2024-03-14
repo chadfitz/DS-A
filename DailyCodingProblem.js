0517e37fb736f1e1a6febf1359711024e01c982a
chad fitzgerald <fitzchad@gmail.com>
2024-03-14 16:29:27 -0700

    Solve problem

--- DailyCodingProblem.js
+++ DailyCodingProblem.js
@@ -21,3 +21,24 @@ const twoSum = (list, k) => {
 // console.log(twoSum([10, 12, 15, 17, 21], 5));
 
 // -------------------------------------------------------------------------------------------------------
+// Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+// Follow-up: what if you can't use division?
+const productExceptI = (arr) => {
+  let totalProd = 1;
+
+  const res = [];
+  for (let num of arr) {
+    totalProd *= num;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    res.push(totalProd/arr[i]);
+  }
+
+  return res;
+}
+// console.log(productExceptI([1, 2, 3, 4, 5]));
+// console.log(productExceptI([3, 2, 1]));
