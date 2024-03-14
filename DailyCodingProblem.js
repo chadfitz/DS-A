25fdd46385a9684cae326389def9c8720fedf44a
chad fitzgerald <fitzchad@gmail.com>
2024-03-14 16:38:29 -0700

    Optimize solution

--- DailyCodingProblem.js
+++ DailyCodingProblem.js
@@ -26,16 +26,19 @@ const twoSum = (list, k) => {
 // For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
 // Follow-up: what if you can't use division?
-const productExceptI = (arr) => {
-  let totalProd = 1;
-
+const productExceptI = (nums) => {
   const res = [];
-  for (let num of arr) {
-    totalProd *= num;
+
+  let leftTotal = 1;
+  for (let i = 0; i < nums.length; i++) {
+    res[i] = leftTotal;
+    leftTotal *= nums[i];
   }
 
-  for (let i = 0; i < arr.length; i++) {
-    res.push(totalProd/arr[i]);
+  let rightTotal = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    res[i] = rightTotal;
+    rightTotal *= nums[i];
   }
 
   return res;
