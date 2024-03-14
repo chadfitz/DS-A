93b973e4ac1e6d3e835bc1675dc2f47dedbfe2b3
chad fitzgerald <fitzchad@gmail.com>
2024-03-13 17:22:58 -0700

    Solve problem

--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -497,3 +497,19 @@ var merge = function(intervals) {
 
     return res;
 };
+
+// 56. Merge Intervals
+var merge = function(intervals) {
+    let sortedIntervals = intervals.sort((a, b) => a[0] - b[0])
+    const res = [intervals[0]];
+
+    for (let i = 1; i < sortedIntervals.length; i++) {
+        if (res[res.length - 1][1] >= sortedIntervals[i][0]) {
+            res[res.length - 1] = [res[res.length - 1][0], Math.max(res[res.length - 1][1], sortedIntervals[i][1])]
+        } else {
+            res.push(sortedIntervals[i]);
+        }
+    }
+
+    return res;
+};
