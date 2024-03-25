5cb984423b56c3bc05bd6421e50ea0312d78949c
chad fitzgerald <fitzchad@gmail.com>
2024-03-25 11:46:38 -0700

    Solve problem

--- DailyCodingProblem.js
+++ DailyCodingProblem.js
@@ -188,6 +188,32 @@ const largestNonAdjacentSum = (nums) => {
 
 // -------------------------------------------------------------------------------------------------------
 // Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
-const jobScheduler = (f, n) {
+const jobScheduler = (f, n) => {
   setTimeout(f, n);
 }
+const functionF = () => {console.log("fart")}
+// jobScheduler(functionF, 1000);
+
+// -------------------------------------------------------------------------------------------------------
+// Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+
+// For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+// Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+
+// trie data structure
+
+// -------------------------------------------------------------------------------------------------------
+// There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways 
+// you can climb the staircase. The order of the steps matters.
+
+// For example, if N is 4, then there are 5 unique ways:
+
+// 1, 1, 1, 1
+// 2, 1, 1
+// 1, 2, 1
+// 1, 1, 2
+// 2, 2
+// What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? 
+// For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+
--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -676,3 +676,28 @@ var mergeTwoLists = function(list1, list2) {
 
     return res.next;
 };
+
+// 21. Merge Two Sorted Lists
+var mergeTwoLists = function(list1, list2) {
+    let res = new ListNode();
+    let current = res;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    if (list1) {
+        current.next = list1;
+    } else {
+        current.next = list2;
+    }
+
+    return res.next;
+};
