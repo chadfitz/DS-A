4bb9d2245a17c35d979e8187337310af957f4be4
chad fitzgerald <fitzchad@gmail.com>
2024-04-02 11:25:25 -0700

    Solve problems

--- AllLeetCodeProblems.js
+++ AllLeetCodeProblems.js
@@ -165,6 +165,18 @@ var removeDuplicates = function(nums) {
   }
   return nums.length;
 };
+// 26. Remove Duplicates from Sorted Array
+    // 2 pointer solution (better)
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            nums[i + 1] = nums[j];
+            i++;
+        }
+    }
+    return i + 1;
+};
 
 // 27. Remove Element
 var removeElement = function(nums, val) {
