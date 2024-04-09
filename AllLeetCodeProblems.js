9f803d52ee564a11f6bf3f8522cc8b5e8cc8a4c9
chad fitzgerald <fitzchad@gmail.com>
2024-04-09 09:08:44 -0700

    Solve problem

--- AllLeetCodeProblems.js
+++ AllLeetCodeProblems.js
@@ -847,3 +847,19 @@ var pivotArray = function(nums, pivot) {
   }
   return lessThan.concat(equal, greaterThan)
 };
+
+219. Contains Duplicate II
+var containsNearbyDuplicate = function(nums, k) {
+    if (nums.length <= 1 || k < 1) return false;
+
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        // can't just do if (map.get(nums[i])) because won't run if index is 0
+        if (map.get(nums[i]) !== undefined) {
+            if (i - map.get(nums[i]) <= k) return true;
+        }
+        map.set(nums[i], i);
+    }
+
+    return false;
+};
