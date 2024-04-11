49a8e01ce9d6cf9b5304630b8eb5231300964adc
chad fitzgerald <fitzchad@gmail.com>
2024-04-11 15:17:25 -0700

    Solve problem

--- RecentLeetCodeProblems.js
+++ RecentLeetCodeProblems.js
@@ -1031,3 +1031,17 @@ var reorderList = function(head) {
         backward = backwardNext;
     }
 };
+
+// 83. Remove Duplicates from Sorted List
+var deleteDuplicates = function(head) {
+    let current = head;
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+};
