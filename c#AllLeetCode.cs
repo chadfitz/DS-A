0fbe39f61385e43b79c973156b7fa1d2f05aae65
chad fitzgerald <fitzchad@gmail.com>
2024-04-17 12:08:33 -0700

    Solve problem

--- c#AllLeetCode.cs
+++ c#AllLeetCode.cs
@@ -29,3 +29,32 @@ public class Solution {
         return true;
     }
 }
+
+// 13. Roman to Integer
+public class Solution {
+    public int RomanToInt(string s) {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        dict.Add('I', 1);
+        dict.Add('V', 5);
+        dict.Add('X', 10);
+        dict.Add('L', 50);
+        dict.Add('C', 100);
+        dict.Add('D', 500);
+        dict.Add('M', 1000);
+
+    int res = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (i < s.Length - 1 && dict[s[i]] < dict[s[i + 1]])
+        {
+            res -= dict[s[i]];
+        }
+        else
+        {
+            res += dict[s[i]];
+        }
+    }
+
+    return res;
+    }
+}
