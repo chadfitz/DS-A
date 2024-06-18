0ca4e039a443daccf574d3c3ca6d4f8d3f12f7e0
chad fitzgerald <fitzchad@gmail.com>
2024-06-18 13:19:15 -0700

    Solve problem

--- LeetCodePython.py
+++ LeetCodePython.py
@@ -10,3 +10,19 @@ class Solution(object):
             if intStr[i] != intStr[len(intStr) - 1 - i]: return False
 
         return True
+    
+# 14. Lonest Common Prefix
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        result = ""
+
+        strs.sort()
+        for i in range(0, len(strs[0])):
+            if strs[0][i] != strs[-1][i]: return result
+            result += strs[0][i]
+
+        return result
