51f05c89346fc76b5aa3be25490302a9e2f6fd47
chad fitzgerald <fitzchad@gmail.com>
2024-07-02 13:31:03 -0700

    Solve problem

--- RecentLeetCodeJava.java
+++ RecentLeetCodeJava.java
@@ -20,3 +20,16 @@ class Solution {
         return stack.empty();
     }
 }
+
+// 9. Palindrome Number
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        char[] nums = String.valueOf(x).toCharArray();
+
+        for(int i = 0; i < nums.length/2; i++) {
+            if (nums[i] != nums[nums.length - 1 - i]) return false;
+        }
+        return true;
+    }
+}
